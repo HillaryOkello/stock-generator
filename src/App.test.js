47a2a1015b49850704c944fade1ac42a02eaebb2
Hillary Okello <hillariouskelly@outlook.com>
2021-08-24 09:03:11 +0300

    add asset folder

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './components/App';
 
 test('renders learn react link', () => {
   const { getByText } = render(
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../assets/App.css';
 
 function App() {
   return (
